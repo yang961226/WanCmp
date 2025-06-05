0d94cad09dd7effd11cb3a55699e186a7d346996
yangmaoting <524250484@qq.com>
2025-06-05 19:30:15 +0800

    feat-引入首页Banner和登录注册弹窗
    - 首页新增Banner轮播图展示
    - 新增登录注册弹窗，通过BottomSheet实现
    - 新增“回到顶部”悬浮按钮，优化长列表浏览体验

--- composeApp/src/commonMain/kotlin/com/sundayting/wancmp/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sundayting/wancmp/screen/home/HomeScreen.kt
@@ -1,19 +1,39 @@
 package com.sundayting.wancmp.screen.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.filled.PersonAdd
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -22,12 +42,24 @@ import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.sundayting.wancmp.screen.home.tab.home_tab.HomeTab
 import com.sundayting.wancmp.screen.home.tab.mine_tab.MineTab
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.stringResource
+import wancmp.composeapp.generated.resources.Res
+import wancmp.composeapp.generated.resources.login_title
+import wancmp.composeapp.generated.resources.register_title
 
 class HomeScreen : Screen {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
+
+        var showLoginSheet by remember { mutableStateOf(false) }
+        val sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = true
+        )
+        val scope = rememberCoroutineScope()
+
         TabNavigator(HomeTab) {
             Scaffold(
                 topBar = {
@@ -70,7 +102,7 @@ class HomeScreen : Screen {
                         )
                         NavigationBarItem(
                             selected = tabNavigator.current == MineTab,
-                            onClick = { tabNavigator.current = MineTab },
+                            onClick = { showLoginSheet = true },
                             icon = {
                                 Icon(
                                     painter = MineTab.options.icon!!,
@@ -83,8 +115,82 @@ class HomeScreen : Screen {
                         )
                     }
                 }
-            ) {paddingValues->
-                Box(Modifier.padding(paddingValues)){
+            ) { paddingValues ->
+                if (showLoginSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showLoginSheet = false
+                        },
+                        sheetState = sheetState
+                    ) {
+
+                        val pagerState = rememberPagerState { 2 }
+
+                        val icons = remember {
+                            listOf(
+                                Icons.Filled.PersonAdd,
+                                Icons.AutoMirrored.Filled.Login
+                            )
+                        }
+
+                        val contentDescriptions = listOf(
+                            stringResource(Res.string.register_title), // 仍然为可访问性提供描述
+                            stringResource(Res.string.login_title)
+                        )
+
+                        SingleChoiceSegmentedButtonRow(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            space = 20.dp
+                        ) {
+                            val selectedIndex = pagerState.currentPage
+                            icons.forEachIndexed { index, icon ->
+                                SegmentedButton(
+                                    shape = SegmentedButtonDefaults.itemShape(
+                                        index = index,
+                                        count = 2
+                                    ),
+                                    onClick = {
+                                        scope.launch {
+                                            pagerState.animateScrollToPage(index)
+                                        }
+                                    },
+                                    selected = index == selectedIndex,
+                                    label = {
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.padding(horizontal = 20.dp)
+                                        ) {
+                                            Icon(
+                                                imageVector = icon,
+                                                contentDescription = contentDescriptions[index]
+                                            )
+
+                                            Spacer(Modifier.width(5.dp))
+
+                                            Text(
+                                                contentDescriptions[index]
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        }
+
+                        HorizontalPager(
+                            state = pagerState,
+                            modifier = Modifier.fillMaxWidth(),
+                            userScrollEnabled = false
+                        ) {
+                            Box(
+                                Modifier.fillMaxWidth().aspectRatio(1f),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text("当前页面：${it}")
+                            }
+                        }
+                    }
+                }
+                Box(Modifier.padding(paddingValues)) {
                     CurrentTab()
                 }
             }
--- composeApp/src/commonMain/kotlin/com/sundayting/wancmp/screen/home/tab/home_tab/HomeTab.kt
+++ composeApp/src/commonMain/kotlin/com/sundayting/wancmp/screen/home/tab/home_tab/HomeTab.kt
@@ -1,6 +1,16 @@
 package com.sundayting.wancmp.screen.home.tab.home_tab
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,9 +18,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,17 +31,24 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import coil3.compose.AsyncImage
 import com.sundayting.wancmp.widgets.LoadStateHostContainer
 import com.sundayting.wancmp.widgets.LoadingMoreWidget
 import com.sundayting.wancmp.widgets.toLoadingMoreState
@@ -37,6 +57,7 @@ import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import wancmp.composeapp.generated.resources.Res
 import wancmp.composeapp.generated.resources.ic_home
+import wancmp.composeapp.generated.resources.ic_loading_pic
 import wancmp.composeapp.generated.resources.tab_home_title
 
 object HomeTab : Tab {
@@ -61,71 +82,153 @@ object HomeTab : Tab {
     override fun Content() {
         val screenModel = koinScreenModel<HomeTabScreenModel>()
         val state = screenModel.state
-
-        PullToRefreshBox(
-            isRefreshing = state.isRefreshing,
-            onRefresh = screenModel::refresh
-        ) {
-            LoadStateHostContainer(
-                modifier = Modifier.fillMaxSize(),
-                state = state,
-                needShowRefreshing = false,
+        val listState = rememberLazyListState()
+        val scope = rememberCoroutineScope()
+        val showFab by remember {
+            derivedStateOf {
+                listState.firstVisibleItemIndex > 5
+            }
+        }
+        BoxWithConstraints(Modifier.fillMaxSize()) {
+            val preferredItemWidth = maxWidth / 2
+            PullToRefreshBox(
+                isRefreshing = state.isRefreshing,
+                onRefresh = screenModel::refresh
             ) {
-                val listState = rememberLazyListState()
-                LazyColumn(
-                    Modifier.fillMaxSize(),
-                    state = listState
+                LoadStateHostContainer(
+                    modifier = Modifier.fillMaxSize(),
+                    state = state,
+                    needShowRefreshing = false,
                 ) {
-                    itemsIndexed(
-                        items = state.articleList,
-                        contentType = { _, _ -> 1 },
-                        key = { _, article -> article.id }
-                    ) { index, article ->
-                        ListItem(
-                            overlineContent = { Text("${article.author}・${article.date}") },
-                            headlineContent = {
-                                Text(
-                                    text = article.title,
-                                    modifier = Modifier.padding(vertical = 6.dp)
-                                )
-                            },
-                            supportingContent = { Text("${article.superChapter.name}・${article.chapter.name}") },
-                            trailingContent = {
-                                IconButton(
-                                    onClick = {
+                    LazyColumn(
+                        Modifier.fillMaxSize(),
+                        state = listState
+                    ) {
+                        if (state.bannerList.isNotEmpty()) {
+                            item(key = "banner", contentType = "banner") {
+                                Column{
+                                    val carouselState = rememberCarouselState { state.bannerList.size }
+                                    HorizontalMultiBrowseCarousel(
+                                        state = carouselState,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(vertical = 8.dp),
+                                        preferredItemWidth = preferredItemWidth,
+                                        itemSpacing = 16.dp,
+                                        contentPadding = PaddingValues(horizontal = 16.dp)
+                                    ) { i ->
+                                        val item = state.bannerList[i]
+                                        AsyncImage(
+                                            model = item.imagePath,
+                                            contentDescription = "banner：${item.title}",
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .aspectRatio(2f / 1f)
+                                                .maskClip(MaterialTheme.shapes.extraLarge)
+                                                .maskBorder(
+                                                    BorderStroke(
+                                                        0.5.dp,
+                                                        MaterialTheme.colorScheme.outline
+                                                    ), MaterialTheme.shapes.extraLarge
+                                                ),
+                                            contentScale = ContentScale.Crop,
+                                            placeholder = painterResource(Res.drawable.ic_loading_pic),
+                                            fallback = painterResource(Res.drawable.ic_loading_pic),
+                                        )
+                                    }
+                                    HorizontalDivider()
+                                }
 
-                                    },
-                                ) {
-                                    Icon(
-                                        if (article.isLike) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
-                                        contentDescription = null,
-                                        tint = if (article.isLike) MaterialTheme.colorScheme.primary else LocalContentColor.current
+                            }
+                        }
+                        itemsIndexed(
+                            items = state.articleList,
+                            contentType = { _, _ -> 1 },
+                            key = { _, article -> article.id }
+                        ) { index, article ->
+                            ListItem(
+                                overlineContent = { Text("${article.author}・${article.date}") },
+                                headlineContent = {
+                                    Text(
+                                        text = article.title,
+                                        modifier = Modifier.padding(vertical = 6.dp)
                                     )
-                                }
-                            },
-                            modifier = Modifier.clickable(
-                                interactionSource = null,
-                                indication = ripple()
-                            ) {
+                                },
+                                supportingContent = { Text("${article.superChapter.name}・${article.chapter.name}") },
+                                trailingContent = {
+                                    IconButton(
+                                        onClick = {
 
-                            }
-                        )
-                        HorizontalDivider()
-                    }
-                    if (state.articleList.isNotEmpty()) {
-                        item {
-                            LaunchedEffect(state.articleList.size) {
-                                screenModel.loadMore()
-                                launch {
-                                    listState.scrollToItem(listState.layoutInfo.totalItemsCount - 1)
+                                        },
+                                    ) {
+                                        Icon(
+                                            if (article.isLike) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
+                                            contentDescription = null,
+                                            tint = if (article.isLike) MaterialTheme.colorScheme.primary else LocalContentColor.current
+                                        )
+                                    }
+                                },
+                                modifier = Modifier.clickable(
+                                    interactionSource = null,
+                                    indication = ripple()
+                                ) {
+
                                 }
-                            }
-                            LoadingMoreWidget(
-                                Modifier.fillMaxWidth(),
-                                state = state.toLoadingMoreState()
                             )
+                            HorizontalDivider()
+                        }
+                        if (state.articleList.isNotEmpty()) {
+                            item {
+                                LaunchedEffect(state.articleList.size) {
+                                    screenModel.loadMore()
+                                    launch {
+                                        listState.scrollToItem(listState.layoutInfo.totalItemsCount - 1)
+                                    }
+                                }
+                                LoadingMoreWidget(
+                                    Modifier.fillMaxWidth(),
+                                    state = state.toLoadingMoreState()
+                                )
+                            }
                         }
                     }
+                }
+            }
+
+
+            AnimatedVisibility(
+                visible = showFab,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 30.dp, end = 20.dp),
+                enter = fadeIn() + scaleIn(
+                    initialScale = 0.1f,
+                ),
+                exit = fadeOut() + scaleOut(
+                    targetScale = 0.1f,
+                )
+            ) {
+
+
+                FloatingActionButtonDefaults.elevation()
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            listState.animateScrollToItem(0) // 点击时，平滑滚动到列表顶部
+                        }
+                    },
+                    elevation = FloatingActionButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 6.dp,
+                        hoveredElevation = 6.dp,
+                        focusedElevation = 6.dp
+
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowUp, // 使用向上的箭头图标
+                        contentDescription = "回到顶部" // 为可访问性添加描述
+                    )
                 }
             }
         }
